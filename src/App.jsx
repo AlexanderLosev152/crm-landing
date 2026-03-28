5d0f4341c4aa4d5d6a74317e398c83b8d88cca91
Alexander <losev.pavlovo152@gmail.com>
2026-03-28 07:19:38 +0300

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from './components/layout/Header/Header';
 
-import './App.scss';
-import { Bg } from './components/ui/Bg/Bg';
 import { Hero } from './components/layout/Hero/Hero';
 import { SectionTitle } from './components/ui/SectionTitle/SectionTitle';
+
+import './App.scss';
 
 export const App = () => {
 	return (
